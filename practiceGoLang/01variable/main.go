c8009a64afe1d27da3d28d6a12d0203221f74cc8
hemanthkumarkotee <hemanthkumarkotee@gmail.com>
2026-03-14 09:26:48 +0530

    -added mvariableData function

--- practiceGoLang/01variable/main.go
+++ practiceGoLang/01variable/main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-func variableData() {
+func main() {
+	mvariableData()
+}
+
+func mvariableData() {
 	// var companyName string = "TechM"
 	// fmt.Println("CompanyName:" + companyName)
 	// var companyID int = 12345
@@ -10,11 +14,12 @@ func variableData() {
 	// var companyRevenue float64 = 123.456
 	// fmt.Println("CompanyRevenue:", companyRevenue)
 	var (
-		empName  = "hemanth"
-		empName  = "hemanth"
-		salaryPA = 150000.50
+		empName      = "hemanth"
+		salaryPA     = 150000.45
+		empName1     = "hari"
+		emp1salaryPA = 160000
 	)
 
-	fmt.Printf("%s getting %.2f$ of salaryPA\n", empName, salaryPA)
-
+	fmt.Printf("%s getting %.2f of salaryPA\n", empName, salaryPA)
+	fmt.Printf("%s getting %d$ of salaryPA\n", empName1, emp1salaryPA)
 }
